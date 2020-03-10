58fa1cb764d48999cdd66c7bbe21d6e91ff5250f
siberiacalling <smirnovasiberia@gmail.com>
2020-03-10 14:41:24 +0300

    Two servers.

--- proxy.js
+++ proxy.js
@@ -4,27 +4,16 @@ const http = require('http');
 const https = require('https');
 const urlapi = require('url');
 
-
-const baseAddress = 8080;
 const httpAddress = 3001;
 const httpsAddress = 3002;
 const httpsOptions = {
-  key: fs.readFileSync('./key.pem'),
-  cert: fs.readFileSync('./cert.pem')
+  key: fs.readFileSync('./conf/localhost+1-key.pem'),
+  cert: fs.readFileSync('./conf/localhost.pem')
 };
 
-const tcpConnection = (conn) => {
-  conn.once('data', function (buf) {
-    // A TLS handshake record starts with byte 22.
-    const address = (buf[0] === 22) ? httpsAddress : httpAddress;
-    const proxy = net.createConnection(address, () => {
-      proxy.write(buf);
-      conn.pipe(proxy).pipe(conn);
-    });
-  });
-}
 
 const httpConnection = (req, res) => {
+  console.log("http");
   const currentUrl = urlapi.parse(req.url);
   const options = {
     host: currentUrl.hostname,
@@ -46,13 +35,26 @@ const httpConnection = (req, res) => {
 }
 
 const httpsConnection = (req, res) => {
-  console.log("HTTPS");
+  console.log("https");
+  // res.setHeader('Content-Type', 'text/html');
+  // res.setHeader('X-Foo', 'bar');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('ok\n');
+  // const currentUrl = urlapi.parse(req.url);
+  // const options = {
+  //   host: currentUrl.hostname,
+  //   port: currentUrl.port,
+  //   path: currentUrl.pathname,
+  //   method: req.method,
+  //   headers: req.headers,
+  // };
 
-  res.writeHead(200, { 'Content-Length': '5' });
-  res.end('HTTPS');
+  // const serverRequest = https.request(options, (serverResponse) => {
+  //   res.writeHead(serverResponse.statusCode, serverResponse.headers);
+  //   serverResponse.pipe(res);
+  // });
+  // req.pipe(serverRequest);
 }
 
-net.createServer(tcpConnection).listen(baseAddress);
 http.createServer(httpConnection).listen(httpAddress);
 https.createServer(httpsOptions, httpsConnection).listen(httpsAddress);
-
